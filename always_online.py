655222e9702b0cb938560367137455f8e1637264
陈志鸿 <chen_zhihong@outlook.com>
2024-07-06 19:31:39 +0800

    Win系统下ping -n指定回显请求数，而非-c 增加SSID识别

--- always_online.py
+++ always_online.py
@@ -1,30 +1,33 @@
 import os
 import time
+import subprocess
 from configparser import ConfigParser
 from BitSrunLogin.LoginManager import LoginManager
 
+def get_ssid():
+    result = subprocess.run(['netsh', 'wlan', 'show', 'interfaces'], capture_output=True, text=True)
+    for line in result.stdout.split('\n'):
+        if 'SSID' in line:
+            ssid = line.split(':')[1].strip()
+            return ssid
+    return None
+
 def is_connect_internet(testip):
-    status = os.system(u"ping {} -c 8".format(testip))
+    status = os.system(u"ping {} -n 8".format(testip))
     return status == 0
 
 def always_login(username, password, testip, checkinterval):
     lm = LoginManager()
     login = lambda : lm.login(username=username, password=password)
     timestamp = lambda : print(time.asctime(time.localtime(time.time())))
-
-    timestamp()
-    try:
-        login()
-    except Exception:
-        pass
     while 1:
-        time.sleep(checkinterval) 
-        if not is_connect_internet(testip):
+        if (not is_connect_internet(testip)) and (get_ssid() == "BIT-Web"):
             timestamp()
             try:
                 login()
             except Exception:
                 pass
+        time.sleep(checkinterval) 
         
 if __name__ == "__main__":
     config = ConfigParser()
@@ -32,5 +35,5 @@ if __name__ == "__main__":
     username = config['Auth']['username']
     password = config['Auth']['password']
     testip = '114.114.114.114'
-    checkinterval = 1 * 60
+    checkinterval = 5
     always_login(username, password, testip, checkinterval)
